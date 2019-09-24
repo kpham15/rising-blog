3a18c2e8c242ad361d9f97df3f2c77b7dae8e1c4
Kris Pham <kristopher.pham@gmail.com>
2019-09-23 21:35:57 -0500

    add toms photo

--- src/components/card.js
+++ src/components/card.js
@@ -1,11 +1,16 @@
 import React from "react"
 
 import cardStyles from "./card.module.scss"
+import TomRomay from "../images/tom.jpg"
 
 const Card = () => {
   return (
-    <div className={cardStyles.cardPanel}>
-      <p>This is a player Card</p>
+    <div className={[cardStyles.card].join(" ")}>
+      <h1 className={cardStyles.cardTitle}>Tom Romay</h1>
+      <div className={cardStyles.cardImage}>
+        <img src={TomRomay} alt="" className={cardStyles.cardImage} />
+      </div>
+      <p className={cardStyles.cardContent}>This is Tom Douche Romay</p>
     </div>
   )
 }
